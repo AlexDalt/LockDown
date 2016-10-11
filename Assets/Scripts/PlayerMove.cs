d203984e1a6feb2a12993f037b57abc90704ef5e
Hal Jones <hj12350@bristol.ac.uk>
2016-10-11 17:09:58 +0100

    Added sample symmetric client/server networking

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+	
+	void Update () {
+        if (isLocalPlayer) {
+            transform.Translate(Input.GetAxis("Horizontal") * 0.1f, 0, Input.GetAxis("Vertical") * 0.1f);
+        }
+	}
+}
