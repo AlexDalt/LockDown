632911f5cceb758435a96182e0dffd0abd1473ce
Bhavish Jogeeah <Bhavish Jogeeah>
2016-10-20 19:28:39 +0100

    Created TestInfiltrator gameobject (Infiltrator w/o networking), added PlayerMove script

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerMove : NetworkBehaviour {
-	
-	void Update () {
-        if (isLocalPlayer) {
-            transform.Translate(Input.GetAxis("Horizontal") * 0.1f, 0, Input.GetAxis("Vertical") * 0.1f);
-        }
-	}
+public class PlayerMove : MonoBehaviour {
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("4") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
 }
