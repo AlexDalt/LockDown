edde23ece541c3ab1a7a47843259d291ef90e558
Bhavish Jogeeah <Bhavish Jogeeah>
2016-10-21 01:20:08 +0100

    Completed simple mouse look and WASD controls for Infiltrator

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,14 +4,57 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerMove : MonoBehaviour {
-    public float speed = 3.0F;
-    public float rotateSpeed = 3.0F;
+
+    public float speed = 3.0F; //Speed of player
+    public float xRotationSpeed = 3.0F; //X-look sensitivity
+    public float yRotationSpeed = 3.0F; //Y-look sensitivity
+
+    //Y-axis look limits
+    public float minimumY = -60.0F;
+    public float maximumY =  60.0F;
+
+    private float rotationY;
+    private Quaternion originalRotation;
+
+    void Start()
+    {
+        originalRotation = Camera.main.transform.localRotation;
+    }
     void Update()
     {
+        //Movement code
         CharacterController controller = GetComponent<CharacterController>();
-        transform.Rotate(0, Input.GetAxis("4") * rotateSpeed, 0);
+
+        //Get forward and back speeds, controller compatible.
         Vector3 forward = transform.TransformDirection(Vector3.forward);
-        float curSpeed = speed * Input.GetAxis("Vertical");
-        controller.SimpleMove(forward * curSpeed);
+        float forwardSpeed = speed * Input.GetAxisRaw("Vertical");
+
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float strafeSpeed = speed * Input.GetAxisRaw("Horizontal");
+
+        controller.SimpleMove(right * strafeSpeed);
+        controller.SimpleMove(forward * forwardSpeed);
+
+        //X-rotation for player, camera moves as it is parented to the transform of the player
+        transform.Rotate(0, Input.GetAxis("Mouse X") * xRotationSpeed, 0);
+        
+        //Y-rotation for main camera
+        rotationY += Input.GetAxis("Mouse Y") * yRotationSpeed;
+        rotationY = ClampAngle(rotationY, minimumY, maximumY);
+        Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+        Camera.main.transform.localRotation = originalRotation * yQuaternion;
+
+
+    }
+
+    //Taken from MouseLook.cs in Unity Standard Assets
+    //Prevents the player from looking too far up or down
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+         angle += 360F;
+        if (angle > 360F)
+         angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
     }
 }
