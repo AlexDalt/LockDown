db3dc762a96cf5dafc6c4446f8cc9563a8fee2f6
Hal Jones <hj12350@bristol.ac.uk>
2016-10-21 22:23:24 +0100

    Camera overlay is now placer over each camera instance, including the main camera

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -14,7 +14,16 @@ public class UIController : MonoBehaviour {
     public GameObject roleUI;
     public GameObject cameraUI;
 
+    public GameObject cameraOverlayPrefab;
+    public GameObject cameraGridUI;
+
+    public GameObject mainUI;
+
+    private CameraOverlay mainCameraOverlay;
+
     public List<Camera> cameras = new List<Camera>();
+
+    private List<CameraOverlay> cameraOverlays = new List<CameraOverlay>();
 
     public void ShowRoleUI(bool active) {
         roleUI.SetActive(active);
@@ -30,22 +39,56 @@ public class UIController : MonoBehaviour {
         Debug.Log("Screens detected: " + Display.displays.Length);
 
         if (active) {
-            cameraUI.SetActive(true);
+            //cameraUI.SetActive(true);
 
             if (Display.displays.Length > 1) {
+                //If a second display is present, push camera grid to it
+
                 Display.displays[1].Activate();
                 Display.displays[1].SetRenderingResolution(1920, 1080);
+
+                cameraGridUI.SetActive(true);
+                cameraGridUI.GetComponent<Canvas>().targetDisplay = 1;
+
+                //Display main camera under canvas 0
+                mainCameraOverlay = Instantiate(cameraOverlayPrefab, mainUI.transform).GetComponent<CameraOverlay>();
+                mainCameraOverlay.Init("Main Camera", true);
+
                 for (int i = 0; i < cameras.Count; i++) {
-                        cameras[i].rect = new Rect((i % 2) * 0.5f, 0.5f - ((i / 2) * 0.5f), 0.5f, 0.5f);
-                        cameras[i].gameObject.SetActive(true);
-                        cameras[i].targetDisplay = 1;
+                    cameras[i].rect = new Rect((i % 2) * 0.5f, 0.5f - ((i / 2) * 0.5f), 0.5f, 0.5f);
+                    cameras[i].gameObject.SetActive(true);
+                    cameras[i].targetDisplay = 1;
+
+                    GameObject newOverlay = Instantiate(cameraOverlayPrefab, cameraGridUI.transform);
+                    CameraOverlay newOverlayScript = newOverlay.GetComponent<CameraOverlay>();
+
+                    newOverlayScript.Init(i, cameras[i].name, 2, 2, i);
+                    newOverlayScript.OnClick += ChangeCamera;
+
+                    cameraOverlays.Add(newOverlayScript);
                 }
             }
             else {
+                //If there's only a single display, display cameras in column on right
+
                 Camera.main.GetComponent<Camera>().rect = new Rect(0, 0, 0.75f, 1);
+                cameraGridUI.SetActive(true);
+
+                //Add main camera
+                mainCameraOverlay = Instantiate(cameraOverlayPrefab, cameraGridUI.transform).GetComponent<CameraOverlay>();
+                mainCameraOverlay.Init("Main Camera", false);
+
                 for (int i = 0; i < cameras.Count; i++) {
                     cameras[i].rect = new Rect(0.75f, 0.75f - (i * 0.25f), 0.25f, 0.25f);
                     cameras[i].gameObject.SetActive(true);
+
+                    GameObject newOverlay = Instantiate(cameraOverlayPrefab, cameraGridUI.transform);
+                    CameraOverlay newOverlayScript = newOverlay.GetComponent<CameraOverlay>();
+
+                    newOverlayScript.Init(i, cameras[i].name, i);
+                    newOverlayScript.OnClick += ChangeCamera;
+
+                    cameraOverlays.Add(newOverlayScript);
                 }
             }
         }
@@ -71,6 +114,8 @@ public class UIController : MonoBehaviour {
     public void ChangeCamera(int id) {
         Camera.main.transform.position = cameras[id].transform.position;
         Camera.main.transform.rotation = cameras[id].transform.rotation;
+
+        mainCameraOverlay.CameraName = cameras[id].name;
     }
 
     public void ChooseRoleInfiltrator() {
