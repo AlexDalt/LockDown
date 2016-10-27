d09ab1496ff49a079f53796d688150ebc3427c7f
Jon Lao <jl14834@my.bristol.ac.uk>
2016-10-27 16:51:55 +0100

    Vertical camera rotation added

---
+++ Assets/Scripts/DroneCamController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates drone camera up and down without rotating the drone itself
+/// </summary>
+
+public class DroneCamController : MonoBehaviour {
+    public int minAngle = 0;
+    public int maxAngle = 90;
+	public void Up()
+
+    {
+        if((gameObject.transform.localEulerAngles.x >= minAngle +1) && (gameObject.transform.localEulerAngles.x <= maxAngle))
+        {
+            transform.Rotate(Vector3.left);
+        }
+        
+    }
+    
+    public void Down()
+    {
+        if ((gameObject.transform.localEulerAngles.x >= minAngle) && (gameObject.transform.localEulerAngles.x <= maxAngle -1)) { 
+            transform.Rotate(Vector3.right);
+        }
+    }
+}
--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls movement of drone and rotation of its camera using WASD, QE, RF
+/// </summary>
 public class DroneController : MonoBehaviour {
 
     private UIController uiController;
     private GameController gameController;
+    private DroneController droneController;
 
+    public float rotationSpeed = -1.5f;
     public float height = 0f;
     public int id;
+    public DroneCamController droneCamController;
+    public Camera childCam; 
 
 
     void Update() {
-        //if (gameController.cameras[id].gameObject.activeSelf == true) { 
+         //if (childCam.gameObject.activeSelf == true) { 
         transform.Translate(Input.GetAxis("Horizontal") * 0.1f, 0, Input.GetAxis("Vertical") * 0.1f);
         if (Input.GetKey(KeyCode.Q)) 
-            transform.Rotate(Vector3.down);
+            transform.Rotate(0,rotationSpeed,0,Space.World);
         if (Input.GetKey(KeyCode.E))
-            transform.Rotate(Vector3.up);        
-    // }
+            transform.Rotate(0,-(rotationSpeed),0,Space.World);
+        if (Input.GetKey(KeyCode.R))
+            droneCamController.Up();
+        if (Input.GetKey(KeyCode.F))
+            droneCamController.Down();    
+      //   }
     }
 }
