18c1283d84f6334f1ef6d46e3b21822b84767a20
Oscar Bailey <oscar.bailey.2014@my.bristol.ac.uk>
2016-11-03 16:26:36 +0000

    Door implements IInteractable plus general code cleanup

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour {
+/// <summary>
+/// Door class controls the movement of the door panels.
+/// The displacement variable is controlled via Mecanim (Unity's animation system)
+/// </summary>
+public class Door : MonoBehaviour, IInteractable {
     public GameObject leftPanel;
     public GameObject rightPanel;
     public float displacement = 0;
-
-    float distance;
+    
     Animator animator;
     Vector3 leftPanelInitialPosition;
     Vector3 rightPanelInitialPosition;
@@ -23,16 +26,14 @@ public class Door : MonoBehaviour {
     void Start () {
         animator = GetComponent<Animator>();
 
+        // Use the panels' relative intial positions to find their open positions
         leftPanelInitialPosition = leftPanel.transform.position;
         rightPanelInitialPosition = rightPanel.transform.position;
 
-        // Find the movement vector and normalise
-        Vector3 rightDirection = (rightPanel.transform.position - leftPanel.transform.position);
-        distance = rightDirection.magnitude;
-        rightDirection.Normalize();
+        Vector3 distanceVector = (rightPanel.transform.position - leftPanel.transform.position);
 
-        leftPanelOpenDisplacement = ((-rightDirection) * distance);
-        rightPanelOpenDisplacement = (rightDirection * distance);
+        leftPanelOpenDisplacement = -distanceVector;
+        rightPanelOpenDisplacement = distanceVector;
     }
 
     void Update() {
@@ -40,7 +41,30 @@ public class Door : MonoBehaviour {
         rightPanel.transform.position = rightPanelInitialPosition + (displacement * rightPanelOpenDisplacement);
     }
 
+    /// <summary>
+    /// Method only present for debugging
+    /// </summary>
     public void ToggleDoor() {
         Open = !Open;
+    }
+
+    // IInteractable Method
+    public string[] GetOptions(Role role) {
+        return new string[] { "Open", "Close" };
+    }
+
+    // IInteractable Method
+    public bool Interact(Role role, int index) {
+        switch(index) {
+            case 0:
+                Open = true;
+                break;
+            case 1:
+                Open = false;
+                break;
+            default:
+                return false;
+        }
+        return true;
     }
 }
